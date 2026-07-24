2ad4d226dbe44853f412242453aa4482fc265a1d
Rafi Ferdos <rafiferdos@gmail.com>
2026-07-25 00:08:24 +0600

    feat: update Prisma schema and migration files for User model and database setup

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,5 +1,5 @@
+import { PrismaClient } from '@/generated/prisma/client.js'
 import { PrismaPg } from '@prisma/adapter-pg'
-import { PrismaClient } from '@prisma/client/extension'
 import 'dotenv/config'
 
 const connectionString = `${process.env.DATABASE_URL}`
