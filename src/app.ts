93cd5e95c367b373321a36c494b9479f75743b0e
Rafi Ferdos <rafiferdos@gmail.com>
2026-07-26 15:31:03 +0600

    style(app): reorder imports and format code for consistency

--- src/app.ts
+++ src/app.ts
@@ -1,26 +1,30 @@
-import type { Application } from 'express'
-import express from 'express'
-import cors from 'cors'
 import cookieParser from 'cookie-parser'
-import type { Request, Response } from 'express'
+import cors from 'cors'
+import type { Application, Request, Response } from 'express'
+import express from 'express'
 import config from './config/index.js'
+import { AuthRoutes } from './modules/auth/auth.route.js'
 
 const app: Application = express()
 
 app.use(
-	cors({
-		origin: config.app_url,
-		credentials: true
-	})
+  cors({
+    origin: config.app_url,
+    credentials: true
+  })
 )
-
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 app.use(cookieParser())
 
 app.get('/', (_req: Request, res: Response) => {
-	res.send('Hello World!')
+  res.send('Hello World!')
 })
+
+/* --------------------------------------------------------------- */
+/*                         Auth Routes                             */
+/* --------------------------------------------------------------- */
+app.use('/api/auth', AuthRoutes)
 
 export default app
