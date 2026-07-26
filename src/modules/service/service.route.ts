a42060f9b4374e026d8acbb20d25234327470cf1
Rafi Ferdos <rafiferdos@gmail.com>
2026-07-27 02:21:03 +0600

    feat(service): add authentication middleware to service creation route

--- src/modules/service/service.route.ts
+++ src/modules/service/service.route.ts
@@ -1,9 +1,11 @@
+import { UserRole } from '@/generated/prisma/enums.js'
+import { auth } from '@/middlewares/auth.js'
 import { Router } from 'express'
 import { serviceController } from './service.controller.js'
 
 const router = Router()
 
-router.post('/', serviceController.createService)
+router.post('/', auth(UserRole.TECHNICIAN), serviceController.createService)
 router.get('/', serviceController.getAllServices)
 
 export const serviceRoutes = router
