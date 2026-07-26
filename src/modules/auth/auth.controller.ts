35bfd0ed257e3fa7233db7ea67dc26e3bbf934dd
Rafi Ferdos <rafiferdos@gmail.com>
2026-07-26 15:11:35 +0600

    feat(auth): implement login and refresh token functionality in AuthControllers

---
+++ src/modules/auth/auth.controller.ts
@@ -0,0 +1,61 @@
+import catchAsync from '@/utils/catchAsync.js'
+import sendResponse from '@/utils/sendResponse.js'
+import type { NextFunction, Request, Response } from 'express'
+import status from 'http-status'
+import { AuthServices } from './auth.service.js'
+
+const loginUser = catchAsync(async (req: Request, res: Response) => {
+  const payload = req.body
+  const result = await AuthServices.login(payload)
+
+  res.cookie('refreshToken', result.refreshToken, {
+    httpOnly: true,
+    secure: false, // Set to true in production when using HTTPS
+    sameSite: 'none',
+    maxAge: 7 * 24 * 60 * 60 * 1000 // 7 days
+  })
+
+  res.cookie('accessToken', result.accessToken, {
+    httpOnly: true,
+    secure: false, // Set to true in production when using HTTPS
+    sameSite: 'none',
+    maxAge: 15 * 60 * 1000 // 15 minutes
+  })
+
+  sendResponse(res, {
+    statusCode: status.OK,
+    message: 'User logged in successfully',
+    data: result
+  })
+})
+
+const refreshToken = catchAsync(
+  async (req: Request, res: Response, next: NextFunction) => {
+    const { refreshToken } = req.cookies
+    if (!refreshToken) {
+      return res
+        .status(status.UNAUTHORIZED)
+        .json({ message: 'Refresh token not found' })
+    }
+
+    const { accessToken } = await AuthServices.refreshToken(refreshToken)
+
+    res.cookie('accessToken', accessToken, {
+      httpOnly: true,
+      secure: false, // Set to true in production when using HTTPS
+      sameSite: 'none',
+      maxAge: 15 * 60 * 1000 // 15 minutes
+    })
+
+    sendResponse(res, {
+      statusCode: status.OK,
+      message: 'Access token refreshed successfully',
+      data: { accessToken }
+    })
+  }
+)
+
+export const AuthControllers = {
+  login: loginUser,
+  refreshToken
+}
