fb6017e26a1b75065f69f1189afeaa7a4a59fc8d
Rafi Ferdos <rafiferdos@gmail.com>
2026-07-27 02:46:00 +0600

    feat(user): add banUserInDB function to update user status to banned

--- src/modules/user/user.service.ts
+++ src/modules/user/user.service.ts
@@ -1,3 +1,4 @@
+import { UserStatus } from '@/generated/prisma/enums.js'
 import { prisma } from '@/lib/prisma.js'
 
 const getAllUsersFromDB = async () => {
@@ -15,6 +16,15 @@ const getAllUsersFromDB = async () => {
   return users
 }
 
+const banUserInDB = async (userId: string) => {
+  const bannedUser = await prisma.user.update({
+    where: { id: userId },
+    data: { status: UserStatus.BANNED }
+  })
+  return bannedUser
+}
+
 export const userService = {
-  getAll: getAllUsersFromDB
+  getAll: getAllUsersFromDB,
+  ban: banUserInDB
 }
