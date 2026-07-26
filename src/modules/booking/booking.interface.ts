f8e74cdc332445d55e0db82026abc2b6f6105ebf
Rafi Ferdos <rafiferdos@gmail.com>
2026-07-27 04:20:21 +0600

    feat(booking): add IBookingStatus interface to define booking status

--- src/modules/booking/booking.interface.ts
+++ src/modules/booking/booking.interface.ts
@@ -1,5 +1,11 @@
+import type { BookingStatus } from '@/generated/prisma/enums.js'
+
 export interface IBookingPayload {
   technicianId: string
   serviceId: string
   scheduleDate: string
+}
+
+export interface IBookingStatus {
+  status: BookingStatus
 }
