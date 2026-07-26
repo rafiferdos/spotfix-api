72e585971e2b5a53e405c7bb5c8235d17ea58720
Rafi Ferdos <rafiferdos@gmail.com>
2026-07-27 01:01:54 +0600

    feat(category): add category controller with create and getAll functions

---
+++ src/modules/category/category.controller.ts
@@ -0,0 +1,32 @@
+import catchAsync from '@/utils/catchAsync.js'
+import sendResponse from '@/utils/sendResponse.js'
+import type { Request, Response } from 'express'
+import status from 'http-status'
+import { categoryService } from './category.service.js'
+
+const createCategory = catchAsync(async (req: Request, res: Response) => {
+  const payload = req.body
+
+  const newCategory = await categoryService.create(payload)
+
+  sendResponse(res, {
+    statusCode: status.CREATED,
+    message: 'Category created successfully',
+    data: newCategory
+  })
+})
+
+const getAllCategories = catchAsync(async (req: Request, res: Response) => {
+  const categories = await categoryService.getAll()
+
+  sendResponse(res, {
+    statusCode: status.OK,
+    message: 'Categories retrieved successfully',
+    data: categories
+  })
+})
+
+export const categoryController = {
+  create: createCategory,
+  getAll: getAllCategories
+}
