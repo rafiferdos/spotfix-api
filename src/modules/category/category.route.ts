e5c16d9d55d4dd60dc32f9b5bc094aa13752dd00
Rafi Ferdos <rafiferdos@gmail.com>
2026-07-27 02:09:02 +0600

    feat(category): add admin authentication to category creation route

--- src/modules/category/category.route.ts
+++ src/modules/category/category.route.ts
@@ -1,9 +1,11 @@
+import { UserRole } from '@/generated/prisma/enums.js'
+import { auth } from '@/middlewares/auth.js'
 import { Router } from 'express'
 import { categoryController } from './category.controller.js'
 
 const router = Router()
 
-router.post('/', categoryController.create)
+router.post('/', auth(UserRole.ADMIN), categoryController.create)
 router.get('/', categoryController.getAll)
 
 export const categoryRoutes = router
