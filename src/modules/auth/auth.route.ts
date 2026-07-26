957d51a10303b6bf3c9beaf70ec6ff2a985e1e9a
Rafi Ferdos <rafiferdos@gmail.com>
2026-07-26 15:11:39 +0600

    fix(auth): correct typo in AuthControllers import in auth.route.ts

--- src/modules/auth/auth.route.ts
+++ src/modules/auth/auth.route.ts
@@ -1,7 +1,8 @@
 import { Router } from "express";
+import { AuthControllers } from "./auth.controller.js";
 
 const router = Router()
 
-router.post('/login', AuthContoller.login)
+router.post('/login', AuthControllers.login)
 
 export const AuthRoutes = router
