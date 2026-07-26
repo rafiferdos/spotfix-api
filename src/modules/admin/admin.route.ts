dc0f9c2ae03e3a1f5d7b9648cf1c7f26a3c8bc5f
Rafi Ferdos <rafiferdos@gmail.com>
2026-07-27 02:43:42 +0600

    feat(admin): add user route to retrieve all users for admin

--- src/modules/admin/admin.route.ts
+++ src/modules/admin/admin.route.ts
@@ -2,10 +2,19 @@ import { UserRole } from '@/generated/prisma/enums.js'
 import { auth } from '@/middlewares/auth.js'
 import { Router } from 'express'
 import { categoryController } from '../category/category.controller.js'
+import { userController } from '../user/user.controller.js'
 
 const router = Router()
 
+/* --------------------------------------------------------------- */
+/*                        Category Routes                          */
+/* --------------------------------------------------------------- */
 router.post('/categories', auth(UserRole.ADMIN), categoryController.create)
 router.get('/categories', auth(UserRole.ADMIN), categoryController.getAll)
+
+/* --------------------------------------------------------------- */
+/*                        User Routes                             */
+/* --------------------------------------------------------------- */
+router.get('/users', auth(UserRole.ADMIN), userController.getAll)
 
 export const adminRoutes = router
