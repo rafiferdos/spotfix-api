22b63aefaeb00c1559e01cecf3387c321344c905
Rafi Ferdos <rafiferdos@gmail.com>
2026-07-26 15:39:33 +0600

    refactor(auth): streamline user authentication and registration logic

--- src/modules/auth/auth.service.ts
+++ src/modules/auth/auth.service.ts
@@ -5,113 +5,110 @@ import { JwtUtils } from '@/utils/jwt.js'
 import bcrypt from 'bcryptjs'
 import status from 'http-status'
 import type { JwtPayload } from 'jsonwebtoken'
-import type { ILoginCredentials } from './auth.interface.js'
+import type {
+  ILoginCredentials,
+  IRegisterUserPayload
+} from './auth.interface.js'
 
 const loginUserIntoDB = async (payload: ILoginCredentials) => {
-	const { email, password } = payload
+  const { email, password } = payload
 
-	const user = await prisma.user.findUniqueOrThrow({
-		where: { email }
-	})
+  const user = await prisma.user.findUniqueOrThrow({
+    where: { email }
+  })
 
-	if (!user) throw new Error('User not found')
+  if (!user) throw new Error('User not found')
 
-	const isPasswordMatched = await bcrypt.compare(password, user.password)
-	if (!isPasswordMatched) throw new Error('Invalid password')
+  const isPasswordMatched = await bcrypt.compare(password, user.password)
+  if (!isPasswordMatched) throw new Error('Invalid password')
 
-	// const { password: _password, ...userWithoutPassword } = user
-	// return userWithoutPassword
+  const jwtPayload = {
+    id: user.id,
+    name: user.name,
+    email: user.email,
+    role: user.role
+  }
 
-	const jwtPayload = {
-		id: user.id,
-		name: user.name,
-		email: user.email,
-		role: user.role
-	}
+  const { accessToken, refreshToken } = JwtUtils.createAuthTokens(jwtPayload, {
+    accessSecret: config.jwtSecret,
+    accessExpiresIn: config.jwtExpiresIn,
+    refreshSecret: config.jwtRefreshSecret,
+    refreshExpiresIn: config.jwtRefreshExpiresIn
+  })
 
-	const { accessToken, refreshToken } = JwtUtils.createAuthTokens(jwtPayload, {
-		accessSecret: config.jwtSecret,
-		accessExpiresIn: config.jwtExpiresIn,
-		refreshSecret: config.jwtRefreshSecret,
-		refreshExpiresIn: config.jwtRefreshExpiresIn
-	})
-
-	return {
-		accessToken,
-		refreshToken
-	}
+  return {
+    accessToken,
+    refreshToken
+  }
 }
 
 const refreshToken = async (token: string) => {
-    const decoded = JwtUtils.verifyToken<JwtPayload>(token, config.jwtRefreshSecret)
+  const decoded = JwtUtils.verifyToken<JwtPayload>(
+    token,
+    config.jwtRefreshSecret
+  )
 
-    const { id } = decoded
+  const { id } = decoded
 
-    const user = await prisma.user.findUnique({
-        where: { id }
-    })
+  const user = await prisma.user.findUnique({
+    where: { id }
+  })
 
-    if (!user) throw new AppError(status.NOT_FOUND, 'User not found')
+  if (!user) throw new AppError(status.NOT_FOUND, 'User not found')
 
-    const jwtPayload = {
-        id,
-        name: user.name,
-        email: user.email,
-        role: user.role
-    }
+  const jwtPayload = {
+    id,
+    name: user.name,
+    email: user.email,
+    role: user.role
+  }
 
-    const { accessToken } = JwtUtils.createAuthTokens(jwtPayload, {
-        accessSecret: config.jwtSecret,
-        accessExpiresIn: config.jwtExpiresIn,
-        refreshSecret: config.jwtRefreshSecret,
-        refreshExpiresIn: config.jwtRefreshExpiresIn
-    })
+  const { accessToken } = JwtUtils.createAuthTokens(jwtPayload, {
+    accessSecret: config.jwtSecret,
+    accessExpiresIn: config.jwtExpiresIn,
+    refreshSecret: config.jwtRefreshSecret,
+    refreshExpiresIn: config.jwtRefreshExpiresIn
+  })
 
-    return { accessToken }
+  return { accessToken }
 }
 
-const registerUserIntoDB = async (payload: IReg) => {
-	const { name, email, password, profilePhoto } = payload
+const registerUserIntoDB = async (payload: IRegisterUserPayload) => {
+  const { name, email, password } = payload
 
-	const user = await prisma.user.findUnique({
-		where: { email }
-	})
-	if (user) throw new AppError(status.CONFLICT, 'User with this email already exists')
+  const user = await prisma.user.findUnique({
+    where: { email }
+  })
+  if (user)
+    throw new AppError(status.CONFLICT, 'User with this email already exists')
 
-	const passwordHash = await bcrypt.hash(
-		password,
-		Number(config.bcryptSaltRounds)
-	)
+  const passwordHash = await bcrypt.hash(
+    password,
+    Number(config.bcryptSaltRounds)
+  )
 
-	const newUser = await prisma.user.create({
-		data: {
-			name,
-			email,
-			password: passwordHash,
-			profile: {
-				create: {
-					profilePhoto: profilePhoto || null
-				}
-			}
-		}
-	})
+  const newUser = await prisma.user.create({
+    data: {
+      name,
+      email,
+      password: passwordHash
+    }
+  })
 
-	const result = await prisma.user.findUnique({
-		where: {
-			id: newUser.id,
-			email: newUser.email || email
-		},
-		include: {
-			profile: true
-		},
-		omit: {
-			password: true
-		}
-	})
-	return result
+  const result = await prisma.user.findUnique({
+    where: {
+      id: newUser.id,
+      email: newUser.email || email
+    },
+    omit: {
+      password: true
+    }
+  })
+  return result
 }
 
 export const AuthServices = {
-	login: loginUserIntoDB,
-	refreshToken
+  login: loginUserIntoDB,
+  refreshToken,
+  register: registerUserIntoDB
 }
