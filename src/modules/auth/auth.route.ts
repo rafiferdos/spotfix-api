290da758d1e612e5ba31ef0a603124acba6e1679
Rafi Ferdos <rafiferdos@gmail.com>
2026-07-26 15:51:04 +0600

    feat(auth): add getMe route to retrieve user details

--- src/modules/auth/auth.route.ts
+++ src/modules/auth/auth.route.ts
@@ -6,5 +6,6 @@ const router = Router()
 router.post('/login', AuthControllers.login)
 router.post('/refresh-token', AuthControllers.refreshToken)
 router.post('/register', AuthControllers.register)
+router.get('/me', AuthControllers.getMe)
 
 export const AuthRoutes = router
