53bc2d11425f041dbe2b13dca787f7b39c774840
Rafi Ferdos <rafiferdos@gmail.com>
2026-07-26 15:38:41 +0600

    refactor(auth): update user role type in registration payload to use UserRole enum

--- src/modules/auth/auth.interface.ts
+++ src/modules/auth/auth.interface.ts
@@ -1,9 +1,11 @@
+import type { UserRole } from '@/generated/prisma/enums.js'
+
 export interface IRegisterUserPayload {
   name: string
   email: string
   password: string
   phone?: string
-  role?: 'CUSTOMER' | 'PROVIDER' | 'ADMIN'
+  role?: UserRole
   address?: string
   profileImage?: string
 }
