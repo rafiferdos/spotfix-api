d2552aeca1d12cd7f86f58991fc0961de18f0166
Rafi Ferdos <rafiferdos@gmail.com>
2026-07-27 02:34:25 +0600

    feat(category): remove authentication middleware from category creation route

--- src/modules/category/category.route.ts
+++ src/modules/category/category.route.ts
@@ -1,11 +1,8 @@
-import { UserRole } from '@/generated/prisma/enums.js'
-import { auth } from '@/middlewares/auth.js'
 import { Router } from 'express'
 import { categoryController } from './category.controller.js'
 
 const router = Router()
 
-router.post('/', auth(UserRole.ADMIN), categoryController.create)
 router.get('/', categoryController.getAll)
 
 export const categoryRoutes = router
