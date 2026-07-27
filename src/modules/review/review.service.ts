337e067d4dbc98dccbf612442ad7920a26b73ea2
Rafi Ferdos <rafiferdos@gmail.com>
2026-07-27 20:34:32 +0600

    fix(review): update booking retrieval to handle not found case and remove customerId from review creation

--- src/modules/review/review.service.ts
+++ src/modules/review/review.service.ts
@@ -4,10 +4,15 @@ import status from 'http-status'
 import type { IReviewPayload } from './review.interface.js'
 
 const createReviewIntoDB = async (userId: string, payload: IReviewPayload) => {
-  const booking = await prisma.booking.findUniqueOrThrow({
+  const booking = await prisma.booking.findUnique({
     where: { id: payload.bookingId },
     include: { review: true }
   })
+  if (!booking)
+    throw new AppError(
+      status.NOT_FOUND,
+      'Booking not found. Please check the booking ID.'
+    )
 
   if (booking.customerId !== userId)
     throw new AppError(
@@ -28,8 +33,8 @@ const createReviewIntoDB = async (userId: string, payload: IReviewPayload) => {
     data: {
       rating: payload.rating,
       comment: payload.comment,
-      bookingId: payload.bookingId,
-      customerId: userId
+      bookingId: payload.bookingId
+      // customerId: userId
     }
   })
   return newReview
