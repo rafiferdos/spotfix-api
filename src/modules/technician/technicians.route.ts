b46a7308b491eee5de8f9f4f6f17545ec8f2a457
Rafi Ferdos <rafiferdos@gmail.com>
2026-07-27 20:48:13 +0600

    feat(technician): add route to retrieve technician profile with reviews

--- src/modules/technician/technicians.route.ts
+++ src/modules/technician/technicians.route.ts
@@ -4,5 +4,6 @@ import { technicianController } from './technician.controller.js'
 const router = Router()
 
 router.get('/', technicianController.getAllTechnicians)
+router.get('/:id', technicianController.getProfileWithReviews)
 
 export const technicianPublicRoutes = router
