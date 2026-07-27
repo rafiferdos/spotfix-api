f6a053c704fc0f822874857ad4db07784a4e4226
Rafi Ferdos <rafiferdos@gmail.com>
2026-07-27 20:28:39 +0600

    feat(review): add authentication middleware for review creation route

--- src/modules/review/review.route.ts
+++ src/modules/review/review.route.ts
@@ -1,8 +1,10 @@
+import { UserRole } from '@/generated/prisma/enums.js'
+import { auth } from '@/middlewares/auth.js'
 import { Router } from 'express'
 import { reviewController } from './review.controller.js'
 
 const router = Router()
 
-router.post('/', reviewController.create)
+router.post('/', auth(UserRole.CUSTOMER), reviewController.create)
 
 export const reviewRoutes = router
