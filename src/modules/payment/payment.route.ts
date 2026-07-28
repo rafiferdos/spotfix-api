27eabff9e1a46fe9781c0334f768b3115bf90a15
Rafi Ferdos <rafiferdos@gmail.com>
2026-07-29 03:58:16 +0600

    refactor(payment): simplify webhook route and update endpoint to '/confirm'

--- src/modules/payment/payment.route.ts
+++ src/modules/payment/payment.route.ts
@@ -1,16 +1,12 @@
 import { UserRole } from '@/generated/prisma/enums.js'
 import { auth } from '@/middlewares/auth.js'
-import express, { Router } from 'express'
+import { Router } from 'express'
 import { paymentController } from './payment.controller.js'
 
 const router = Router()
 
 router.post('/checkout', auth(UserRole.CUSTOMER), paymentController.checkout)
 
-router.post(
-  '/webhook',
-  express.raw({ type: 'application/json' }),
-  paymentController.webhook
-)
+router.post('/confirm', paymentController.webhook)
 
 export const paymentRoutes = router
