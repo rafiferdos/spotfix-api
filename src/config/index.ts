8ebbd90c8d0194916d108e50e50633d3205f0f92
Rafi Ferdos <rafiferdos@gmail.com>
2026-08-12 02:01:44 +0600

    feat: update configuration to include Cloudinary settings

--- src/config/index.ts
+++ src/config/index.ts
@@ -4,17 +4,20 @@ import path from 'path'
 dotenv.config({ path: path.join(process.cwd(), '.env') })
 
 export default {
-	port: process.env.PORT || 5000,
-	database_url: process.env.DATABASE_URL || '',
-	bcryptSaltRounds: process.env.BCRYPT_SALT_ROUNDS || 10,
-	jwtSecret: process.env.JWT_SECRET || '',
-	jwtExpiresIn: process.env.JWT_EXPIRES_IN || '1h',
-	jwtRefreshSecret: process.env.JWT_REFRESH_SECRET || '',
-	jwtRefreshExpiresIn: process.env.JWT_REFRESH_EXPIRES_IN || '7d',
-	app_url: process.env.APP_URL || 'http://localhost:3000',
-	stripe_product_id: process.env.STRIPE_PRODUCT_ID || '',
-	stripe_price_id: process.env.STRIPE_PRICE_ID || '',
-	stripe_secret_key: process.env.STRIPE_SECRET_KEY || '',
-	stripe_webhook_secret: process.env.STRIPE_WEBHOOK_SECRET || '',
-	isProduction: process.env.NODE_ENV === 'production'
+  port: process.env.PORT || 5000,
+  database_url: process.env.DATABASE_URL || '',
+  bcryptSaltRounds: process.env.BCRYPT_SALT_ROUNDS || 10,
+  jwtSecret: process.env.JWT_SECRET || '',
+  jwtExpiresIn: process.env.JWT_EXPIRES_IN || '1h',
+  jwtRefreshSecret: process.env.JWT_REFRESH_SECRET || '',
+  jwtRefreshExpiresIn: process.env.JWT_REFRESH_EXPIRES_IN || '7d',
+  app_url: process.env.APP_URL || 'http://localhost:3000',
+  stripe_product_id: process.env.STRIPE_PRODUCT_ID || '',
+  stripe_price_id: process.env.STRIPE_PRICE_ID || '',
+  stripe_secret_key: process.env.STRIPE_SECRET_KEY || '',
+  stripe_webhook_secret: process.env.STRIPE_WEBHOOK_SECRET || '',
+  isProduction: process.env.NODE_ENV === 'production',
+  cloudinary_cloud_name: process.env.CLOUDINARY_CLOUD_NAME || '',
+  cloudinary_api_key: process.env.CLOUDINARY_API_KEY || '',
+  cloudinary_api_secret: process.env.CLOUDINARY_API_SECRET || ''
 }
