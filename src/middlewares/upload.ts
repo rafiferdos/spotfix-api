de47a985c8cd571043cfe56ad8c2cf0b8ff58dca
Rafi Ferdos <rafiferdos@gmail.com>
2026-08-12 19:55:13 +0600

    feat: update image upload middleware to enforce 2MB size limit and refactor code structure

--- src/middlewares/upload.ts
+++ src/middlewares/upload.ts
@@ -1,12 +1,13 @@
 import { AppError } from '@/utils/appError.js'
+import type { NextFunction, Request, Response } from 'express'
 import status from 'http-status'
 import multer from 'multer'
 
 const storage = multer.memoryStorage()
 
-export const uploadSingleImage = multer({
+const upload = multer({
   storage,
-  limits: { fileSize: 3 * 1024 * 1024 }, // 3MB
+  limits: { fileSize: 2 * 1024 * 1024 }, // 2MB
   fileFilter: (_req, file, cb) => {
     if (!file.mimetype.startsWith('image/')) {
       return cb(
@@ -16,3 +17,22 @@ export const uploadSingleImage = multer({
     cb(null, true)
   }
 }).single('profileImage')
+
+export const uploadSingleImage = (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  upload(req, res, (err: unknown) => {
+    if (err instanceof multer.MulterError) {
+      if (err.code === 'LIMIT_FILE_SIZE') {
+        return next(
+          new AppError(status.BAD_REQUEST, 'Image must be smaller than 2MB')
+        )
+      }
+      return next(new AppError(status.BAD_REQUEST, err.message))
+    }
+    if (err) return next(err)
+    next()
+  })
+}
